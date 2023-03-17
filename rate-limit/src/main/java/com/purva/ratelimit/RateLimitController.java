d7e0c0c454cac196fbd6264d0a6f1a80adf8b4ad
Aureole11 <purvagupta5555@gmail.com>
2023-03-17 17:05:45 +0530

    initial commit

---
+++ rate-limit/src/main/java/com/purva/ratelimit/RateLimitController.java
@@ -0,0 +1,57 @@
+package com.purva.ratelimit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.bucket4j.Bucket;
+
+@RestController
+@RequestMapping("/rate-limit")
+public class RateLimitController {
+
+	@Value("${rate.limit.client.basic}")
+	private String basic;
+	
+	@Value("${rate.limit.client.free}")
+	private String free;
+	
+	@Autowired
+	private RateLimitPricePlanservice ratelimit;
+	
+	private Bucket bucket = null;
+	
+	/**
+	 * to generate token using api
+	 * @return
+	 */
+	@GetMapping("/token-generate/{clientToken}")
+	public 	ResponseEntity<String> generateToken(@PathVariable("clientToken") String clientToken){
+		//refil - rate at which token will be filled in the bucket	
+		//bucket - bucket is interface and its implementation is bucket4j
+		bucket=ratelimit.getPlanServiceBucket(clientToken);
+
+		return new ResponseEntity<String>("Generated Successfully||| \n"+ bucket.toString(), HttpStatus.OK);
+	}
+	
+	/**
+	 * call exact api for demo
+	 * @return
+	 */
+
+	@GetMapping("/demo")
+	public ResponseEntity<String> demo(){
+		if(bucket.tryConsume(1))//try to consume 1 token
+		{
+			System.out.println("=========Api working successfuly=========");
+			return new ResponseEntity<String>("Success", HttpStatus.OK);
+		}
+		System.out.println("=========number of hits exceeded=========");
+		return new ResponseEntity<String>("Too many hits !!! Please try later !!!", HttpStatus.TOO_MANY_REQUESTS);
+	}
+}
---
+++ rate-limit/src/main/java/com/purva/ratelimit/RateLimitPricePlanservice.java
@@ -0,0 +1,47 @@
+package com.purva.ratelimit;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
+import io.github.bucket4j.local.LocalBucket;
+
+/**
+ * provide number of tokens in bucket
+ * according to price plans by customer
+ * @author Niks
+ *
+ */
+
+@Service
+public class RateLimitPricePlanservice {
+
+	@Value("${rate.limit.client.basic}")
+	private String basic;
+	
+	@Value("${rate.limit.client.free}")
+	private String free;
+	
+	public Bucket getPlanServiceBucket(String clientToken) {
+		LocalBucket data = Bucket4j.builder()
+		.addLimit(getClientBandwidth(clientToken))
+		.build();
+		
+		System.out.println(data);
+		return data;
+	}
+
+	private Bandwidth getClientBandwidth(String clientToken) {
+		
+		if(clientToken.equals(basic))//basic plan customers
+		return Bandwidth.classic(5, Refill.intervally(5, Duration.ofMinutes(1)));
+		else if(clientToken.equals(free))//premium customers
+			return Bandwidth.classic(25, Refill.intervally(25, Duration.ofMinutes(1)));
+		return Bandwidth.classic(2, Refill.intervally(2, Duration.ofMinutes(1)));
+	}
+}
